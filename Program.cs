b398cd0b0bc887ab9db806cf38451dfdf6598abd
denkeel <denkeelpro@gmail.com>
2016-10-04 22:22:16 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,69 +10,77 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            int u;
+            u = int.Parse(Console.ReadLine());
+            if (u < 3)
+            {
+                Console.WriteLine("ERROR. U must be more than 2.");
+                return;
+            }
             int x;
             int a, b;
             int l;
             char s = 'x';
             bool f = false;
             char[,] array;
-            array = new char[3, 3];
+            array = new char[u, u];
 
-            for (int i = 0; i < 3; i++)
-                for (int j = 0; j < 3; j++)
+            for (int i = 0; i < u; i++)
+                for (int j = 0; j < u; j++)
                     array[i, j] = '-';
 
-            for (int p = 0; p < 9; p++)
+            for (int p = 0; p < u*u; p++)
             {
                 do
                 {
                     f = false;
                     x = int.Parse(Console.ReadLine());
-                    a = (x - 1) / 3;
-                    b = (x - 1) % 3;
+                    a = (x - 1) / u;
+                    b = (x - 1) % u;
                     if (array[a, b] != '-')
                     {
-                        Console.WriteLine("ERROR");
+                        Console.WriteLine("ERROR. TRY AGAIN.");
                         f = true;
                     }
                 } while (f);
+
                 array[a, b] = s;
 
-                for (int i = 0; i < 3; i++)
+                for (int i = 0; i < u; i++)
                 {
-                    for (int j = 0; j < 3; j++)
+                    for (int j = 0; j < u; j++)
                         Console.Write(array[i, j]);
                     Console.WriteLine();
                 }
 
-                for (int k = 0; k < 3; k++)
+                for (int k = 0; k < u; k++)
                 {
                     l = 0;
-                    while (((l < 2) && (array[k,l]!='-')) && (array[k, l] == array[k, l+1]))
+                    while (((l < (u-1)) && (array[k,l]!='-')) && (array[k, l] == array[k, l+1]))
                         l++;
-                    if (l == 2)
+                    if (l == (u-1))
                         goto Win;
                 }
                 
-                for (int k = 0; k < 3; k++)
+                for (int k = 0; k < u; k++)
                 {
                     l = 0;
-                    while (((l < 2) && (array[l, k] != '-')) && (array[l, k] == array[l + 1, k]))
+                    while (((l < (u-1)) && (array[l, k] != '-')) && (array[l, k] == array[l + 1, k]))
                         l++;
-                    if (l == 2)
+                    if (l == (u-1))
                         goto Win;
                 }
 
                 l = 0;
-                while (((l < 2) && (array[l, l] != '-')) && (array[l, l] == array[l + 1, l + 1]))
+                while (((l < (u-1)) && (array[l, l] != '-')) && (array[l, l] == array[l + 1, l + 1]))
                     l++;
-                if (l == 2)
+                if (l == (u-1))
                     goto Win;
 
                 l = 0;
-                while (((l < 2) && (array[l, 2 - l] != '-')) && (array[l, 2 - l] == array[l + 1, 1 - l]))
+                while (((l < (u-1)) && (array[l, 2 - l] != '-')) && (array[l, 2 - l] == array[l + 1, 1 - l]))
                     l++;
-                if (l == 2)
+                if (l == (u-1))
                     goto Win;
 
                 if (s == 'x')
